2c07f2131712e354c1a5a2c49a272dd89d4bd7b7
nadiabryn <54154226+nadiabryn@users.noreply.github.com>
2019-10-15 14:58:21 +0300

    Create day01.js

---
+++ day01.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/training-js-number-1-create-your-first-js-function-and-print-helloworld/train/javascript
+
+function helloWorld(){
+var str = 'Hello World';
+console.log(str);
+}
+
+helloWorld();
